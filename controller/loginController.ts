620d5681e3bd11d7685f143d96d1cf1a3f406071
wminaung <minwin243@gmail.com>
2023-02-17 01:08:25 +0630

    try to make mvc pattern

--- controller/loginController.ts
+++ controller/loginController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import path from "path";
+import { v4 } from "uuid";
+import { keys, users } from "../routes/loginRouter";
 
 const showLoginPage = (req: Request, res: Response) => {
   const url = path.join(__dirname, "..", "public", "login.html");
@@ -7,4 +9,40 @@ const showLoginPage = (req: Request, res: Response) => {
   res.sendFile(url);
 };
 
-export { showLoginPage };
+const chatkey = (req: Request, res: Response) => {
+  const hasKey = keys.some((key) => {
+    return key.key === req.body.key;
+  });
+  if (hasKey) {
+    res.end();
+  } else {
+    res.redirect("/login");
+  }
+};
+
+const isAuthencate = (req: Request, res: Response) => {
+  const key = v4();
+
+  const { name, email } = req.body;
+
+  const foundUser = users.find(
+    (user) => user.name === name && user.email === email
+  );
+  const existUser = { ...foundUser };
+
+  if (Object.keys(existUser).length < 1) {
+    return res
+      .status(404)
+      .json({ status: "fail", message: "user & email are not exits" });
+  }
+  if (!existUser.id) {
+    return res
+      .status(404)
+      .json({ status: "fail", message: "user id doesn't exits" });
+  }
+  keys.push({ id: keys.length + 1, key: key, user_id: existUser.id });
+
+  res.status(200).json({ status: "success", key: key, message: "authencated" });
+};
+
+export { showLoginPage, chatkey, isAuthencate };
--- routes/loginRouter.ts
+++ routes/loginRouter.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from "express";
 const loginRouter = express.Router();
 import { v4 } from "uuid";
-import { showLoginPage } from "../controller/loginController";
+import {
+  chatkey,
+  isAuthencate,
+  showLoginPage,
+} from "../controller/loginController";
 
 interface Key {
   id: number;
@@ -9,7 +13,7 @@ interface Key {
   key: string;
 }
 
-const keys: Key[] = [] as Key[];
+export const keys: Key[] = [] as Key[];
 
 interface UserInfo {
   id: number;
@@ -17,7 +21,7 @@ interface UserInfo {
   email: string;
 }
 
-const users: UserInfo[] = [
+export const users: UserInfo[] = [
   {
     id: 1,
     name: "admin",
@@ -32,42 +36,8 @@ const users: UserInfo[] = [
 
 loginRouter.get("/login", showLoginPage);
 
-loginRouter.post("/getkey", (req: Request, res: Response) => {
-  const hasKey = keys.some((key) => {
-    return key.key === req.body.key;
-  });
-  if (hasKey) {
-    res.end();
-  } else {
-    res.redirect("/login");
-  }
-});
+loginRouter.post("/chatkey", chatkey);
 
-loginRouter.post("/isAuthencate", (req: Request, res: Response) => {
-  const key = v4();
-
-  const { name, email } = req.body;
-
-  const foundUser = users.find(
-    (user) => user.name === name && user.email === email
-  );
-  const existUser = { ...foundUser };
-  console.log(existUser, "exitUser");
-
-  if (Object.keys(existUser).length < 1) {
-    return res
-      .status(404)
-      .json({ status: "fail", message: "user & email are not exits" });
-  }
-  if (!existUser.id) {
-    return res
-      .status(404)
-      .json({ status: "fail", message: "user id doesn't exits" });
-  }
-  keys.push({ id: keys.length + 1, key: key, user_id: existUser.id });
-  console.log(keys);
-
-  res.status(200).json({ status: "success", key: key, message: "authencated" });
-});
+loginRouter.post("/isAuthencate", isAuthencate);
 
 export { loginRouter };
